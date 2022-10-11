b0c39dbb98c8a33cbf832859efa780bbd060fba1
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-11 14:43:42 -0400

    Replaced text in body with a column

--- flutter_learning1/lib/main.dart
+++ flutter_learning1/lib/main.dart
@@ -5,12 +5,23 @@ void main() => runApp((MyApp()));
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Color.fromARGB(255, 255, 83, 0),
-              title: Text('My First App!'),
-            ),
-            body: Text('Oh wow an app.')));
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 255, 83, 0),
+          title: const Text('My First App!'),
+        ),
+        body: Column(
+          children: const [
+            Text('Yep yep text'),
+            //FloatingActionButton(onPressed: onPressed)
+          ],
+        ),
+      ),
+    );
   }
 }
