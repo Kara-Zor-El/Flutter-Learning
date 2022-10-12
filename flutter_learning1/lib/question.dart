a0cbc8f0bf28e8f27b511423df0f1b83cd88374a
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-12 16:09:21 -0400

    Added a widget file for managing questions

---
+++ flutter_learning1/lib/question.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  String questionText;
+
+  Question(this.questionText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
