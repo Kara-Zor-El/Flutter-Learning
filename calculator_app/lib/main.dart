a2a6c328ec0e4d5ec2138265f7c387258e5ad3a8
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-16 20:23:29 -0400

    Added a button constructor, a few buttons, and a text field.

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,26 +12,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Calculator(title: 'Calculator'),
+      debugShowCheckedModeBanner: false,
+      title: 'Best Calculator!',
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Calculator(),
     );
   }
 }
 
 class Calculator extends StatefulWidget {
-  const Calculator({super.key, required this.title});
-
-  final String title;
+  const Calculator({super.key});
 
   @override
-  State<Calculator> createState() => _MyCalculator();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _MyCalculator extends State<Calculator> {
+class _CalculatorState extends State<Calculator> {
+  Widget buttonConstructor({
+    String text = "",
+    double mL = 10,
+    double mT = 5,
+    double mR = 0,
+    double mB = 0,
+    double pH = 35,
+    double pV = 0,
+  }) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(mL, mT, mR, mB),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: pH, vertical: pV),
+        ),
+        onPressed: null,
+        child: Text(
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          text,
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Awesome Calculator'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            margin: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: const Text(
+                "Nothing So Far!",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.fromLTRB(20, 100, 20, 0),
+            padding: const EdgeInsets.only(bottom: 250),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Row(
+              children: [
+                buttonConstructor(text: "X"),
+                buttonConstructor(text: "÷"),
+                buttonConstructor(text: "("),
+                buttonConstructor(text: ")"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
