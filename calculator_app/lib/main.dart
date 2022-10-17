c992485bd63e97ea696d1e1e7cf9800776c0ce0a
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-17 14:08:44 -0400

    Added all buttons and their functionality
    Except equals.

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,14 +26,17 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  String displayString = "";
   Widget buttonConstructor({
     String text = "",
-    double mL = 10,
+    double mL = 8,
     double mT = 5,
     double mR = 0,
     double mB = 0,
     double pH = 35,
     double pV = 0,
+    bool isEquals = false,
+    bool isCE = false,
   }) {
     return Container(
       margin: EdgeInsets.fromLTRB(mL, mT, mR, mB),
@@ -43,13 +44,29 @@ class _CalculatorState extends State<Calculator> {
         style: ElevatedButton.styleFrom(
           padding: EdgeInsets.symmetric(horizontal: pH, vertical: pV),
         ),
-        onPressed: null,
+        onPressed: !isEquals && !isCE
+            ? () => addToString(text)
+            : isEquals
+                ? null
+                : () => clearEverything(),
         child: Text(
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           text,
         ),
       ),
     );
+  }
+
+  void addToString(String s) {
+    setState(() {
+      displayString += s;
+    });
+  }
+
+  void clearEverything() {
+    setState(() {
+      displayString = "";
+    });
   }
 
   @override
@@ -69,26 +86,63 @@ class _CalculatorState extends State<Calculator> {
             ),
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: const Text(
-                "Nothing So Far!",
+              child: Text(
+                displayString,
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
               ),
             ),
           ),
           Container(
             alignment: Alignment.center,
-            margin: const EdgeInsets.fromLTRB(20, 100, 20, 0),
-            padding: const EdgeInsets.only(bottom: 250),
+            margin: const EdgeInsets.fromLTRB(20, 125, 20, 0),
+            padding: const EdgeInsets.only(bottom: 15),
             decoration: BoxDecoration(
               border: Border.all(width: 2),
               borderRadius: const BorderRadius.all(Radius.circular(10)),
             ),
-            child: Row(
+            child: Column(
               children: [
-                buttonConstructor(text: "X"),
-                buttonConstructor(text: "÷"),
-                buttonConstructor(text: "("),
-                buttonConstructor(text: ")"),
+                Row(
+                  children: [
+                    buttonConstructor(text: "x"),
+                    buttonConstructor(text: "÷"),
+                    buttonConstructor(text: "("),
+                    buttonConstructor(text: ")"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buttonConstructor(text: "7", pV: 10, mT: 15),
+                    buttonConstructor(text: "8", pV: 10, mT: 15),
+                    buttonConstructor(text: "9", pV: 10, mT: 15),
+                    buttonConstructor(text: "-", pH: 32, pV: 20),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buttonConstructor(text: "4", pV: 10, mT: 15),
+                    buttonConstructor(text: "5", pV: 10, mT: 15),
+                    buttonConstructor(text: "6", pV: 10, mT: 15),
+                    buttonConstructor(text: "+", pH: 30, pV: 20),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buttonConstructor(text: "1", pV: 10, mT: 15),
+                    buttonConstructor(text: "2", pV: 10, mT: 15),
+                    buttonConstructor(text: "3", pV: 10, mT: 15),
+                    buttonConstructor(
+                        text: "=", pH: 30, pV: 20, isEquals: true),
+                  ],
+                ),
+                Row(children: [
+                  buttonConstructor(
+                      text: "Clear Everything",
+                      pV: 10,
+                      pH: 84,
+                      mT: 15,
+                      isCE: true),
+                ]),
               ],
             ),
           ),
